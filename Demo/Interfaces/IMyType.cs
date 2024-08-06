3b6c964772727beed6e54ba3f9f1bcd670f88b7a
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-08-06 12:42:04 +0300

    Interface

---
+++ Demo/Interfaces/IMyType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Interfaces
+{
+    interface IMyType
+    {
+        // Default access modifier inside interface is public 
+        // private access modifier is not allowed inside the interface
+
+
+        // What you can write inside the interface
+        // 1. Signature for Property [Name - Type [Get] [Set]] -> Signature for property
+
+
+        public int Id { get; set; }
+        // signature for property because its is inside an interface and not an automatic property
+        // inside class or struct -> automatic property
+        // Compiler will generate private hidden attribute [Backing Field]
+        // inside interface -> signature for property
+
+
+        // 2. Signature for methods [Name - Return Type - Parameters]
+
+        void MyFun(int X);
+        // cannot be private, protected, internal, protected internal, private protected
+        // 
+
+
+        // 3. Default Implmented methods [C# 8.0] feature (.NetCore 3.1 2019)
+
+        public void print()
+        {
+            Console.WriteLine("Hello default implmented method");
+        }
+
+
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Demo
+﻿using Demo.Interfaces;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Interface
+
+            //IMyType myType = new IMyType(); // invalid because its not implmented
+
+            IMyType myType;
+            // you can create a reference but not an object 
+            // CLR will allocated 4 bytes for reference "myType"
+            // references myType refer to null
+            // reference myType can refer to an object from class or struct implment interface
+
+            //myType.Id = 20; // invalid  -> myType refer to null
+            //myType.MyFun(20); // invalid -> myType refer to null
+            // myType.print(); // invalid -> myType refer to null
+
+            //MyType myType01 = new MyType();
+
+            //myType01.Id = 30;
+            //myType01.MyFun(100);
+
+            //myType01.print(); // invalid
+
+            IMyType myType2 = new MyType();
+            myType2.Id = 30;
+            myType2.MyFun(100); // valid because of the inteface reference 
+            myType2.print(); 
+
+            #endregion
         }
     }
 }
